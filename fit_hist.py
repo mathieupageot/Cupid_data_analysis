96fca4fbf1758cda21060a8228b6fac48fd71c6a
mp274748 <mathieu.pageot@cea.fr>
2023-04-21 11:41:12 +0200

    add use value of the second data set

--- fit_hist.py
+++ fit_hist.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-amp=np.array([170,211,231,346,384,435,643])
+#amp=np.array([170,211,231,346,384,435,643]) #for'/Users/mp274748/Documents/20180709_23h07.BINLMO.ntp'
+amp=np.array([142,206,247,316,407,485,712])
 value=np.array([238.6,295,352,510,583,609,911])
+
 z=np.polyfit(amp,value,1)
 p = np.poly1d(z)
 plt.scatter(amp,value)
