ce35298c2a23d9c1695c2e4da10c85ac0f838622
mp274748 <mathieu.pageot@cea.fr>
2023-04-25 12:00:37 +0200

    get the path to data set

--- get_data.py
+++ get_data.py
@@ -2,6 +2,27 @@ import numpy as np
 def ntd_array(path):
     A=np.loadtxt(path)
     return A
+def get_path():
+    i = input("Data_set: ")
+
+    if i == 3:
+        path = '/Users/mp274748/Documents/data_arg/second_set/'
+        filename = '20211125_00h43.BINLMO21.2.ntp'
+        filename_light = '20211125_00h43.BINLD21.2.ntp'
+        filename_trigheat = '20211125_00h43.BINLD21.2_trigheat.ntp'
+    if i == 2:
+        path = '/Users/mp274748/Documents/data_arg/first_set2/'
+        filename = '20180709_23h07.BINLMO.ntp'
+        filename_light = '20180709_23h07.BINLD.ntp'
+        filename_trigheat = '20180709_23h07.BINLD_trigheat.ntp'
+    if i == 1:
+        path = '/Users/mp274748/Documents/data_arg/first_set/'
+        filename = '20180709_23h07.BINLMO.ntp'
+        filename_light = '20180709_23h07.BINLD.ntp'
+        filename_trigheat = '20180709_23h07.BINLD_trigheat.ntp'
+
+
+    return path,filename,filename_light,filename_trigheat
 
 if __name__=="__main__":
     arr=ntd_array(r'/Users/mp274748/Documents/20180709_23h07.BINLD.ntp')
