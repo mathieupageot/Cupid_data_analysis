741151d5d9946ae0fe2ba3f1113ef8ee9458fa3f
mp274748 <mathieu.pageot@cea.fr>
2023-05-12 16:54:13 +0200

    define a default value for the file choice

--- get_data.py
+++ get_data.py
@@ -3,9 +3,10 @@ def ntd_array(path):
     A=np.loadtxt(path)
     return A
 def get_path():
-    i = input("Data_set: ")
+    i = 3#int(input("Data_set: "))
+    print(i)
 
-    if i == 3:
+    if i == 3: #measurement 0003
         path = '/Users/mp274748/Documents/data_arg/second_set/'
         filename = '20211125_00h43.BINLMO21.2.ntp'
         filename_light = '20211125_00h43.BINLD21.2.ntp'
@@ -14,7 +15,7 @@ def get_path():
         path = '/Users/mp274748/Documents/data_arg/first_set2/'
         filename = '20180709_23h07.BINLMO.ntp'
         filename_light = '20180709_23h07.BINLD.ntp'
-        filename_trigheat = '20180709_23h07.BINLD_trigheat.ntp'
+        filename_trigheat = '20180709_23h07.BINLD_trigheat0.ntp'
     if i == 1:
         path = '/Users/mp274748/Documents/data_arg/first_set/'
         filename = '20180709_23h07.BINLMO.ntp'
@@ -22,7 +23,8 @@ def get_path():
         filename_trigheat = '20180709_23h07.BINLD_trigheat.ntp'
 
 
-    return path,filename,filename_light,filename_trigheat
+
+    return path,filename,filename_light,filename_trigheat,i
 
 if __name__=="__main__":
     arr=ntd_array(r'/Users/mp274748/Documents/20180709_23h07.BINLD.ntp')
