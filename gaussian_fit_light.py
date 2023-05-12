84890a6a7ac336a8573de6b83f67f75a8ae153ef
mp274748 <mathieu.pageot@cea.fr>
2023-05-12 16:53:39 +0200

    creat the landofit fot the muon fit on the light channel

--- gaussian_fit_light.py
+++ gaussian_fit_light.py
@@ -1,25 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import lasso_selection
-import get_data
 from scipy.optimize import curve_fit
+from landaupy import langauss
+from scipy.stats import median_abs_deviation
 
-def gauss(x, H, A, x0, sigma):
-    return H + A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
-fig4, axs4= plt.subplots(1)
-filename_light='/20211125_00h43.BINLD21.2.ntp'
-filename_trigheat='/20211125_00h43.BINLD21.2_trigheat.ntp'
-path = '/Users/mp274748/Documents/data_arg/second_set'
-peakslt=get_data.ntd_array(path + filename_light)
-corlt=peakslt[:,5]
-goodlt = corlt > 0.99
-amplt=peakslt[goodlt,2]
-
-n, bins,patches = axs4.hist(amplt,200)
-bin_center=(bins[:-1]+bins[1:])/2
-parameters, covariance = curve_fit(gauss, bin_center, n,p0=[6,75,13000,2000])
-x_plot=np.linspace(bin_center.min(),bin_center.max(),1000)
-axs4.plot(x_plot,gauss(x_plot,*parameters))
-
-
-plt.show()
+def binned_fit_langauss(samples, bins='auto', nan='remove'):
+	if nan == 'remove':
+		samples = samples[~np.isnan(samples)]
+	hist, bin_edges = np.histogram(samples, bins, density=True)
+	bin_centers = bin_edges[:-1] + np.diff(bin_edges)/2
+	# Add an extra bin to the left:
+	hist = np.insert(hist, 0, sum(samples<bin_edges[0]))
+	bin_centers = np.insert(bin_centers, 0, bin_centers[0]-np.diff(bin_edges)[0])
+	# Add an extra bin to the right:
+	hist = np.append(hist,sum(samples>bin_edges[-1]))
+	bin_centers = np.append(bin_centers, bin_centers[-1]+np.diff(bin_edges)[0])
+	landau_x_mpv_guess = bin_centers[np.argmax(hist)]
+	landau_xi_guess = median_abs_deviation(samples)/5
+	gauss_sigma_guess = landau_xi_guess/10
+	popt, pcov = curve_fit(
+		lambda x, mpv, xi, sigma: langauss.pdf(x, mpv, xi, sigma),
+		xdata = bin_centers,
+		ydata = hist,
+		p0 = [landau_x_mpv_guess, landau_xi_guess, gauss_sigma_guess],
+	)
+	return popt, pcov, hist, bin_centers
