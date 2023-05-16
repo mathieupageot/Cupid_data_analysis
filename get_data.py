79162b3bfac1be4c9da4afe5c24c2ea380570c17
mp274748 <mathieu.pageot@cea.fr>
2023-05-16 10:16:00 +0200

    pick file 2

--- get_data.py
+++ get_data.py
@@ -3,14 +3,14 @@ def ntd_array(path):
     A=np.loadtxt(path)
     return A
 def get_path():
-    i = 3#int(input("Data_set: "))
-    print(i)
+    i = 2#int(input("Data_set: "))
 
     if i == 3: #measurement 0003
         path = '/Users/mp274748/Documents/data_arg/second_set/'
         filename = '20211125_00h43.BINLMO21.2.ntp'
         filename_light = '20211125_00h43.BINLD21.2.ntp'
         filename_trigheat = '20211125_00h43.BINLD21.2_trigheat.ntp'
+
     if i == 2:
         path = '/Users/mp274748/Documents/data_arg/first_set2/'
         filename = '20180709_23h07.BINLMO.ntp'
@@ -21,7 +21,7 @@ def get_path():
         filename = '20180709_23h07.BINLMO.ntp'
         filename_light = '20180709_23h07.BINLD.ntp'
         filename_trigheat = '20180709_23h07.BINLD_trigheat.ntp'
-
+    print('file: '+filename)
 
 
     return path,filename,filename_light,filename_trigheat,i
