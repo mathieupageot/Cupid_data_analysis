347f66f08c91aefb7bdb98a9bc382664fa933ac9
mp274748 <mathieu.pageot@cea.fr>
2023-06-26 17:22:11 +0200

    add a duration computation and delete all the filename since saved in dictionary

--- get_data.py
+++ get_data.py
@@ -1,8 +1,24 @@
 import numpy as np
 import datetime
 import json
-
-
+def find_local_maxima(data):
+    local_maxima = []
+    for i in range(1, len(data) - 1):
+        if data[i] > data[i - 1] and data[i] > data[i + 1]:
+            local_maxima.append(data[i])
+    local_maxima.append(data[-1])
+    return local_maxima
+def get_duration(i,j):
+    path, dictionary = get_path(i, j)
+    filename = dictionary["filename"]
+    try:
+        peaks = ntd_array(path + filename)
+    except TypeError:
+        filename = dictionary["filename_light"]
+        peaks = ntd_array(path + filename)
+    duration = np.sum(find_local_maxima(peaks[:, 0]))
+    update_dict(path + "dictionary.json", "duration", duration)
+    return duration
 def ntd_array(path):
     print(path)
     A = np.loadtxt(path, usecols=(n for n in range(20)))
@@ -33,7 +49,8 @@ def get_values(dictionary,key_list):
             value_set.append(dictionary[key])
     return value_set
 
-def get_path(i=7, j=1, peak=False):
+
+def get_path(i=7, j=1):
     # input("file?")
     j = int(j)
     filenum = i
@@ -41,21 +58,11 @@ def get_path(i=7, j=1, peak=False):
 
     if i == 3:  # measurement 0003
         path = '/Users/mp274748/Documents/data_arg/second_set/'
-        filename = '20211125_00h43.BINLMO21.2.ntp'
-        filename_light = '20211125_00h43.BINLD21.2.ntp'
-        filename_trigheat = '20211125_00h43.BINLD21.2_trigheat.ntp'
     if i == 2:
         path = '/Users/mp274748/Documents/data_arg/first_set2/'
-        filename = '20180709_23h07.BINLMO.ntp'
-        filename_light = '20180709_23h07.BINLD.ntp'
-        filename_trigheat = '20180709_23h07.BINLD_trigheat0.ntp'
     if i == 1:
         path = '/Users/mp274748/Documents/data_arg/first_set/'
-        filename = '20180709_23h07.BINLMO.ntp'
-        filename_light = '20180709_23h07.BINLD.ntp'
-        filename_trigheat = '20180709_23h07.BINLD_trigheat.ntp'
     if i == 4:
-        filename = '20230308_16h15.BIN'
         path = '/Users/mp274748/Documents/data_arg/RUN93/'
         if j == 1:
             name = '60b_ara1'
@@ -73,67 +80,38 @@ def get_path(i=7, j=1, peak=False):
             name = '61b_UV1'
         elif j == 8:
             name = '61b_UV2'
-        else:
-            name = ''
         path += name + '/'
-        filename += name + '.ntp'
-        filename_light = 0
-        filename_trigheat = 0
     # data from edi
     if filenum == 5:
-        filename = 'LoggedData_2021_05_05_17_40_40.NTD_'
         path = '/Users/mp274748/Documents/data_edi/'
         if filebis == 0:
             name = 'RAW'
         if filebis == 1:
             name = 'DIF'
         path += name + '/'
-        filename += name + '.ntp'
         filename_light = 0
         filename_trigheat = 0
     # data form RUN96 measurement 2
     if filenum == 6:
         if filebis == 54:
-            filename_light = '000004_20230606T192243_005_trig4.ntp'
-            path = '/Users/mp274748/Documents/data_arg/RUN96/Measurement2/trig4/'
+            path = '/Users/mp274748/Documents/data_arg/RUN96/Measurement2/5_trig4/'
         elif filebis == 64:
-            filename_light = '000004_20230606T192243_006_trig4.ntp'
-            path = '/Users/mp274748/Documents/data_arg/RUN96/Measurement2/trig4/'
+            path = '/Users/mp274748/Documents/data_arg/RUN96/Measurement2/6_trig4/'
         else:
-            filename_light = '000004_20230606T192243_00' + str(filebis) + '_001.bin.ntp'
             path = '/Users/mp274748/Documents/data_arg/RUN96/Measurement2/channel' + str(filebis) + '/'
-        filename = 0
-        filename_trigheat = 0
     # data form RUN96 measurement 6
     if filenum == 7:
         path = '/Users/mp274748/Documents/data_arg/RUN96/Measurement6/'
         if filebis == 0:
             path += 'LMO18/'
-            filename = '000015_20230609T235012_008.ntp'
-            filename_light = '000015_20230609T235012_001_bis.ntp'
-            filename_trigheat = '000015_20230609T235012_001_trig.ntp'
         elif filebis == 1:
             path += 'LMO26/'
-            filename = '000015_20230609T235012_007.ntp'
-            filename_light = '000015_20230609T235012_002.ntp'
-            filename_trigheat = '000015_20230609T235012_002_trig.ntp'
-    try:
-        print('file: ' + filename)
-    except TypeError:
-        try:
-            print('file: ' + filename_light)
-        except TypeError:
-            print('No file found')
-
-    dict = {"path": path, "filename": filename, "filename_light": filename_light,
-            "filename_trigheat": filename_trigheat}
-    try :
-        for key, value in zip(dict.keys(), dict.values()):
-            update_dict(path + "dictionary.json", key, value)
-    except FileNotFoundError:
-        with open(path + "dictionary.json", 'w') as json_file:
-            json.dump(dict, json_file)
     dictionary = load_dict(path + "dictionary.json")
+    try:
+        duration = dictionary["duration"]
+    except ValueError:
+        duration = get_duration(i,j)
+    print('duration of the run :', str(datetime.timedelta(seconds=duration/5000)))
     return path, dictionary
 
 
@@ -152,8 +130,6 @@ def get_heat(path, filename, p, dict):
     decayt = peaks[:, 12]
     Sm = peaks[:, 9] / amp_stab
     time = peaks[:, 0]
-    print("acquisition duration(h:m:s):",
-          str(datetime.timedelta(hours=np.round((peaks[-1, 0] - peaks[0, 0]) / 5000 / 60 / 60, 2))))
     return E, amp_stab, correlation, TV, riset, decayt, Sm, time
 
 
@@ -164,3 +140,6 @@ def import_light(path, filename_light):
     ampl = peaksl[sel_corr, 2]
     riset = peaksl[sel_corr, 11]
     return ampl,riset
+
+i_s = [1,2,3,4,5,6,7]
+j_s = [[1],[1],[1],[j for j in range(1,9)],[0,1],[54,64,4,5,6],[0,1]]
